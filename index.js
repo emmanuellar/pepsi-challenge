1fc3ad5fda39fa9ec20033718db365f58d4edc12
emmanuellar <emmanuellar805@gmail.com>
2020-12-14 20:48:13 +0100

    done with the UI

---
+++ index.js
@@ -0,0 +1,20 @@
+var container1 = new Vue({
+    el:"#containers",
+    data:{
+        isHere:true
+    }
+})
+
+// var textCont = new Vue({
+//     el:"#text-cont",
+//     data: {
+//         activeClass: 'row'
+//     }
+// })
+var botton = new Vue({
+    el:"#botton",
+    data: {
+        activeColor: '#fff',
+        activePad: 20
+    }
+})
